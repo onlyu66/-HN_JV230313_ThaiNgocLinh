9424f97900e547e3ec62844c0572572cb15c40b4
onlyu66 <ngoclinhthai8@gmail.com>
2024-03-13 22:02:34 +0700

    web_app

--- Exam_JavaCore_Webapp/src/main/java/com/ra/model/entity/Student.java
+++ Exam_JavaCore_Webapp/src/main/java/com/ra/model/entity/Student.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name = "Student")
+@Table(name = "student")
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
